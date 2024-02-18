7da80fcd5a8f21ce9185b9505986563ab36f464a
Andrii Stadnyk <stadnyk.andy@gmail.com>
2024-02-18 22:50:19 +0200

    tw classes added

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,9 +1,6 @@
 import { ImageResponse } from "next/og";
 
-// Route segment config
 export const runtime = "edge";
-
-// Image metadata
 export const size = {
   width: 1200,
   height: 630,
@@ -11,42 +8,56 @@ export const size = {
 export const alt = "My image alt text";
 export const contentType = "image/png";
 
-// Image generation
 export default async function Image() {
   return new ImageResponse(
     (
       <div
-        style={{
-          fontSize: 42,
-          background: "red",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          padding: 64,
-        }}>
+        className={
+          "flex h-full w-full flex-col items-center justify-center bg-red-600 p-10 text-4xl"
+        }>
         <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 40,
-            textAlign: "center",
-            width: "100%",
-            color: "black",
-          }}>
-          <h1
-            style={{
-              fontSize: 72,
-            }}>
-            COrgiMom
-          </h1>
+          className={
+            "flex w-full flex-col items-center justify-center gap-4 text-center text-black"
+          }>
+          <h1 className={"text-6xl"}>COrgiMom</h1>
+          <p>Another looooong text</p>
         </div>
       </div>
     ),
     { ...size },
   );
+}
+
+{
+  /* <div
+style={{
+  fontSize: 42,
+  background: "red",
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  padding: 64,
+}}>
+<div
+  style={{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 40,
+    textAlign: "center",
+    width: "100%",
+    color: "black",
+  }}>
+  <h1
+    style={{
+      fontSize: 72,
+    }}>
+    COrgiMom
+  </h1>
+</div>
+</div> */
 }
