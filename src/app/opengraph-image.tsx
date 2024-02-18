557441a69a94252c1dc3c04ce734347559609a80
Andrii Stadnyk <stadnyk.andy@gmail.com>
2024-02-18 21:37:34 +0200

    fix og

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -17,39 +17,42 @@ export default async function Image() {
   return new ImageResponse(
     (
       <div
-        className={
-          "relative flex h-full w-full flex-col items-center justify-center"
-        }
-        // style={{
-        //   width: "100%",
-        //   height: "100%",
-        //   display: "flex",
-        //   alignItems: "center",
-        //   justifyContent: "center",
-        //   flexDirection: "column",
-        //   position: "relative",
-        // }}
-      >
+        // className={
+        //   "relative flex h-full w-full flex-col items-center justify-center"
+        // }
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          position: "relative",
+        }}>
         <div
-          className={
-            "z-10 flex h-full flex-col items-center justify-center gap-y-7 py-16 text-center"
-          }>
+        // className={
+        //   // "z-10 flex h-full flex-col items-center justify-center gap-y-7 py-16 text-center"
+        // }
+        >
           <h1
-            className={
-              "text-5xl font-semibold text-white md:text-[68px] md:font-bold "
-            }>
+          // className={
+          //   "text-5xl font-semibold text-white md:text-[68px] md:font-bold "
+          // }
+          >
             <span
-              className={
-                "font-caveat text-6xl font-bold text-mainYellow-500 md:text-[88px] md:font-extrabold"
-              }>
+            // className={
+            //   "font-caveat text-6xl font-bold text-mainYellow-500 md:text-[88px] md:font-extrabold"
+            // }
+            >
               Вікторія
             </span>{" "}
             - чарівна кінологиня
           </h1>
           <p
-            className={
-              "mb-7 max-w-[500px] text-lg font-medium text-white md:text-xl"
-            }>
+          // className={
+          //   "mb-7 max-w-[500px] text-lg font-medium text-white md:text-xl"
+          // }
+          >
             Ваш експерт з комфортних відносин між Вами та Вашим песиком.
           </p>
         </div>
