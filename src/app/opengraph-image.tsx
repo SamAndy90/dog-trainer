7e55f396b483a316df13501287c39c4b7c7437f5
Andrii Stadnyk <stadnyk.andy@gmail.com>
2024-02-21 23:42:12 +0200

    og3

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -12,50 +12,37 @@ export default async function Image() {
   return new ImageResponse(
     (
       <div
-        className={
-          "flex h-full w-full flex-col items-center justify-center bg-red-600 p-10 text-4xl"
-        }>
+        style={{
+          fontSize: 42,
+          background: "red",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: 64,
+        }}>
         <div
-          className={
-            "flex w-full flex-col items-center justify-center gap-4 text-center text-black"
-          }>
-          <h1 className={"text-6xl"}>COrgiMom</h1>
-          <p>Another looooong text</p>
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 40,
+            textAlign: "center",
+            width: "100%",
+            color: "black",
+          }}>
+          <h1
+            style={{
+              fontSize: 72,
+            }}>
+            COrgiMom
+          </h1>
         </div>
       </div>
     ),
     { ...size },
   );
 }
-
-// <div
-// style={{
-//   fontSize: 42,
-//   background: "red",
-//   width: "100%",
-//   height: "100%",
-//   display: "flex",
-//   alignItems: "center",
-//   justifyContent: "center",
-//   flexDirection: "column",
-//   padding: 64,
-// }}>
-// <div
-//   style={{
-//     display: "flex",
-//     flexDirection: "column",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     gap: 40,
-//     textAlign: "center",
-//     width: "100%",
-//     color: "black",
-//   }}>
-//   <h1
-//     style={{
-//       fontSize: 72,
-//     }}>
-//     COrgiMom
-//   </h1>
-// </div>
-// </div>
